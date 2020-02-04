3f5f99daedd49690634b623854ad076d2ad50ae4
Julie  McManus <julieannesays@gmail.com>
2020-02-04 19:46:10 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -3,6 +3,17 @@ require 'pry'
 class TicTacToe
   attr_accessor :board
 
+  WIN_COMBINATIONS = [
+  [0,1,2],
+  [3,4,5],
+  [6,7,8],
+  [0,3,6],
+  [1,4,7],
+  [2,5,8],
+  [0,4,8],
+  [6,4,2]
+]
+
   def initialize
     @board = board || Array.new(9, " ")
   end
