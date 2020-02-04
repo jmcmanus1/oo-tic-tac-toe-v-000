879cbb3ebdc199b00d1e6b014233845d967163ff
Julie  McManus <julieannesays@gmail.com>
2020-02-04 20:16:08 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -26,8 +26,8 @@ class TicTacToe
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
 
-  def input_to_index(user_input)
-    user_input.to_i-1
+  def input_to_index(input)
+    input.to_i-1
   end
 
   def move(index, token = "X")
